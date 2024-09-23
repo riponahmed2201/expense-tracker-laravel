918aa6dc8503c7e3ee01f87b6c272a7184239ccc
Md. Ripon Mia <33976306+riponahmed2201@users.noreply.github.com>
2024-09-23 14:22:03 +0600

    category crud star

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,12 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CategoryRequest;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class CategoryController extends Controller
 {
-    public function index()
+    public function index(): View
     {
-        
+        return view('admin.category.index');
+    }
+
+    public function store(CategoryRequest $request): RedirectResponse
+    {
+        try {
+            return back()->with('success', 'Category created successfully!');
+        } catch (\Exception $exception) {
+            return back()->with('error', $exception->getMessage());
+        }
     }
 }
